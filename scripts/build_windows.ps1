e87c17fa17b1c9dd6a050dcb323f1588a22f7492
Andreas Bergmeier <abergmeier@gmx.net>
2020-06-09 20:08:43 +0200

    Try vswhere

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -1,4 +1,3 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
-tree "C:\Program Files (x86)\Microsoft Visual Studio\2017"
-$env:path -split ";"
-msbuild /p:Configuration=Debug CnCRemastered.sln
+. "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
+$msBuildPath = "C:\tools\vswhere.exe" -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe | select-object -first 1
+#& "$msBuildPath" /p:Configuration=Debug CnCRemastered.sln
