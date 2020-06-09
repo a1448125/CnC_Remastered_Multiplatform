8b21ea87f9df8560bb09ff7c2fee366d7ea48662
Andreas Bergmeier <abergmeier@gmx.net>
2020-06-09 17:26:23 +0200

    Revert "Try vcvars64.bat"
    This reverts commit 990a24f19c10ff72b734a28fb38cd5d8489a3161.

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\vcvars64.bat"
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
 $env:path -split ";"
 msbuild /p:Configuration=Debug CnCRemastered.sln
