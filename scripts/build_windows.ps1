31d0d050bd228d432ed06677159d7f31c7095eda
Andreas Bergmeier <abergmeier@gmx.net>
2020-06-09 17:27:44 +0200

    look at tree

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -1,3 +1,4 @@
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
+tree "C:\Program Files (x86)\Microsoft Visual Studio\2017"
 $env:path -split ";"
 msbuild /p:Configuration=Debug CnCRemastered.sln
